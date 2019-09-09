5b928d22b6cd23e88b6bfe198577d21472da5925
Felojcj <felocorrea18@gmail.com>
2019-09-08 22:02:16 -0500

    Updates

---
+++ precache-manifest.c348a2021f9afdf6fcd88ed83a494b05.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "537fb1460df9f65ad1415b5c451e74ae",
+    "url": "/CalculadoraEstadistica/index.html"
+  },
+  {
+    "revision": "f838fe613121fe9d9a30",
+    "url": "/CalculadoraEstadistica/static/css/main.22330dca.chunk.css"
+  },
+  {
+    "revision": "67cd5af332c849f1aad0",
+    "url": "/CalculadoraEstadistica/static/js/2.84137cfe.chunk.js"
+  },
+  {
+    "revision": "f838fe613121fe9d9a30",
+    "url": "/CalculadoraEstadistica/static/js/main.d96b531b.chunk.js"
+  },
+  {
+    "revision": "ed567ba76fa32cab8da3",
+    "url": "/CalculadoraEstadistica/static/js/runtime~main.83c35254.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.689ee072822d8447646cc24817294dec.js"
+  "/CalculadoraEstadistica/precache-manifest.c348a2021f9afdf6fcd88ed83a494b05.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/CalculadoraEstadistica/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
